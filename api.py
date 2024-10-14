096c8074ad66b468bd4f194e67770ccdd4f3fc99
cauaschuch <caua.ferrazza@gmail.com>
2024-10-14 17:04:48 -0300

    att

--- api.py
+++ api.py
@@ -1,13 +1,17 @@
 from mp_api.client import MPRester
 import math
 import numpy as np
+import re
 
 key = "H2RaVIDWeAR6N1y8E9lh9XYqB8mwVog7"
+composto = 'Ag2PHO4'
+simetria = 'Trigonal'
 with MPRester(key) as mpr:
-    docs = mpr.materials.summary.search(formula="Nb6Si7Ni16",crystal_system='Cubic')
+    docs = mpr.materials.summary.search(formula=composto,crystal_system=simetria)
     mpids = [doc.material_id for doc in docs]
     structure = mpr.get_structure_by_material_id(mpids[0])
 
+#INICIO PARTE 2 DO TRABALHO (XU E GADELHA)
 with open('data_MP.txt', 'w') as file:
     file.write(str(structure))
 
@@ -19,18 +23,23 @@ def extrair_parametros_rede(filename):
 
     a, b, c = None, None, None
     alpha, beta, gamma = None, None, None
-
-    for line in lines:
-        if 'abc' in line:
-            parts = line.split(':')
-            a, b, c = map(float, parts[1].split())
-        elif 'angles' in line:
-            parts = line.split(':')
-            alpha, beta, gamma = map(float, parts[1].split())
-
-    return a, b, c, alpha, beta, gamma
-
-a, b, c, alpha, beta, gamma = extrair_parametros_rede('data_MP.txt')
+    #pegar abc
+    parts = lines[2].split(':')
+    a, b, c = map(float, parts[1].split())
+    #pegar angulos
+    parts = lines[3].split(':')
+    alpha, beta, gamma = map(float, parts[1].split())
+    #pegar numero de atomos
+    nat = int(lines[-1].split(' ')[1])+1
+    vetores = []
+    for line in lines[8:]:
+        line = line.split()
+        vetores.append([line[1],float(line[2]),float(line[3]),float(line[4])])
+    tipos_atomicos = set()
+    for vetor in vetores:
+        tipos_atomicos.add(vetor[0])
+    
+    return a, b, c, alpha, beta, gamma,nat,tipos_atomicos,vetores
 def calculo_parametros_lattice(a, b, c, alpha, beta, gamma):
     # converter deg pra rad
     alpha = math.radians(alpha)
@@ -56,9 +65,112 @@ def calculo_parametros_lattice(a, b, c, alpha, beta, gamma):
     sgr = math.sqrt(1 - cgr**2)
 
     return ar,cgr,sgr,cb,sa,ca
+def massa_atomica(elemento):
+    elemento_massa = {
+        "H": 1.00784,
+        "He": 4.002602,
+        "Li": 6.938,
+        "Be": 9.0121831,
+        "B": 10.806,
+        "C": 12.011,
+        "N": 14.0067,
+        "O": 15.999,
+        "F": 18.998403163,
+        "Ne": 20.1797,
+        "Na": 22.98976928,
+        "Mg": 24.305,
+        "Al": 26.9815385,
+        "Si": 28.085,
+        "P": 30.973761998,
+        "S": 32.06,
+        "Cl": 35.45,
+        "Ar": 39.948,
+        "K": 39.0983,
+        "Ca": 40.078,
+        "Sc": 44.955908,
+        "Ti": 47.867,
+        "V": 50.9415,
+        "Cr": 51.9961,
+        "Mn": 54.938044,
+        "Fe": 55.845,
+        "Co": 58.933194,
+        "Ni": 58.6934,
+        "Cu": 63.546,
+        "Zn": 65.38,
+        "Ga": 69.723,
+        "Ge": 72.63,
+        "As": 74.921595,
+        "Se": 78.971,
+        "Br": 79.904,
+        "Kr": 83.798,
+        "Rb": 85.4678,
+        "Sr": 87.62,
+        "Y": 88.90584,
+        "Zr": 91.224,
+        "Nb": 92.90637,
+        "Mo": 95.95,
+        "Tc": 98.0,
+        "Ru": 101.07,
+        "Rh": 102.90550,
+        "Pd": 106.42,
+        "Ag": 107.8682,
+        "Cd": 112.414,
+        "In": 114.818,
+        "Sn": 118.710,
+        "Sb": 121.760,
+        "Te": 127.60,
+        "I": 126.90447,
+        "Xe": 131.293,
+        "Cs": 132.90545196,
+        "Ba": 137.327,
+        "La": 138.90547,
+        "Ce": 140.116,
+        "Pr": 140.90766,
+        "Nd": 144.242,
+        "Pm": 145.0,
+        "Sm": 150.36,
+        "Eu": 151.964,
+        "Gd": 157.25,
+        "Tb": 158.92535,
+        "Dy": 162.500,
+        "Ho": 164.93033,
+        "Er": 167.259,
+        "Tm": 168.93422,
+        "Yb": 173.045,
+        "Lu": 174.9668,
+        "Hf": 178.49,
+        "Ta": 180.94788,
+        "W": 183.84,
+        "Re": 186.207,
+        "Os": 190.23,
+        "Ir": 192.217,
+        "Pt": 195.084,
+        "Au": 196.966569,
+        "Hg": 200.592,
+        "Tl": 204.38,
+        "Pb": 207.2,
+        "Bi": 208.98040,
+        "Th": 232.0377,
+        "Pa": 231.03588,
+        "U": 238.02891
+    }
+    return elemento_massa[elemento]
 
+a, b, c, alpha, beta, gamma, nat, tipos_atomicos, vetores = extrair_parametros_rede('data_MP.txt')
 ar,cgr,sgr,cb,sa,ca = calculo_parametros_lattice(a, b, c, alpha, beta, gamma)
-baserot = np.identity(3)
+diag = np.identity(3)
 stdbase = np.array([[1.0 / ar, -cgr / sgr / ar, cb * a], [0.0, b * sa, b * ca], [0.0, 0.0, c]],dtype=float,)
-base = np.dot(stdbase, baserot)
-print(base)
+base = np.dot(stdbase, diag)
+
+with open(f'{composto}.in', 'w') as file: #montar o .in do composto
+    file.write(f'&CONTROL\n/\n&SYSTEM\nibrav = 0\nnat = {nat}\nntyp ={len(tipos_atomicos)}\n/\n&ELECTRONS\n/\n')
+    file.write("CELL_PARAMETERS angstrom\n")
+    for vetor in base:
+        file.write(f"   {vetor[0]:>10.6f}  {vetor[1]:>10.6f}  {vetor[2]:>10.6f}\n")
+    file.write(f'ATOMIC_SPECIES\n')
+    for elemento in tipos_atomicos:
+        file.write(f'     {elemento} {massa_atomica(elemento)} {elemento}_pseudo\n')
+    file.write('ATOMIC_POSITIONS {crystal}\n')
+    for vetor in vetores:
+        file.write(f' {vetor[0]} {vetor[1]:>10.6f}  {vetor[2]:>10.6f}  {vetor[3]:>10.6f}\n')
+    file.write('K_POINTS automatic')
