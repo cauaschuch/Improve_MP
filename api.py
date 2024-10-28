b1693d3c158b2f875963b52ff8d7279b07d7b93e
cauaschuch <caua.ferrazza@gmail.com>
2024-10-28 12:31:27 -0300

    alteracoes na pt2 + documentação inicial

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from mp_api.client import MPRester
 import math
 import numpy as np
-
+from decimal import Decimal
 
 key = "H2RaVIDWeAR6N1y8E9lh9XYqB8mwVog7"
 
@@ -33,63 +33,122 @@ with MPRester(key) as mpr:
         
         return internacional, estrutura
 
-n,structure = busca_material(composto,s)
+N_internacional,estrutura = busca_material(composto,s)
 
-#INICIO PARTE 2 DO TRABALHO (XU E GADELHA)
 with open('data_MP.txt', 'w') as file:
-    file.write(str(structure))
+    file.write(str(estrutura))
 
 def extrair_parametros_rede(filename):
-    with open(filename, 'r') as file:
-        text = file.read()
+    """
+    Extrai parâmetros de rede e informações sobre o material desejado de um arquivo de texto
+    obtido do Materials Project.
 
+    Args:
+        filename (str): Caminho para o arquivo de texto que contém os dados dos parâmetros de rede e posições atômicas.
+
+    Returns:
+        tuple: Uma tupla contendo:
+            - a (float): Comprimento da aresta `a` da célula unitária.
+            - b (float): Comprimento da aresta `b` da célula unitária.
+            - c (float): Comprimento da aresta `c` da célula unitária.
+            - alpha (float): Ângulo entre as arestas `b` e `c` (em graus).
+            - beta (float): Ângulo entre as arestas `a` e `c` (em graus).
+            - gamma (float): Ângulo entre as arestas `a` e `b` (em graus).
+            - nat (int): Número total de átomos na célula unitária.
+            - tipos_atomicos (set): Conjunto contendo os tipos de átomos únicos presentes no material.
+            - vetores (list): Lista contendo as posições e tipos dos átomos, onde cada elemento é uma lista no formato 
+              `[tipo_atomo, x, y, z]` com as coordenadas `x`, `y`, `z`.
+            - prefix (str): Nome do composto.
+
+    Raises:
+        FileNotFoundError: Se o arquivo especificado não for encontrado.
+    """
+    try:
+        with open(filename, 'r') as file:
+            text = file.read()
+    except FileNotFoundError:
+        print(f"Erro: O arquivo '{filename}' não foi encontrado.")
+        return None
     lines = text.split('\n')
-
     a, b, c = None, None, None
     alpha, beta, gamma = None, None, None
-    #pegar abc
+    
     parts = lines[2].split(':')
     a, b, c = map(float, parts[1].split())
-    #pegar angulos
+    
     parts = lines[3].split(':')
     alpha, beta, gamma = map(float, parts[1].split())
-    #pegar numero de atomos
+    
     nat = int(lines[-1].split()[0])+1
     vetores = []
+
+    prefix = lines[1].split()[-1]
+
     for line in lines[8:]:
         line = line.split()
         vetores.append([line[1],float(line[2]),float(line[3]),float(line[4])])
     tipos_atomicos = set()
+
     for vetor in vetores:
         tipos_atomicos.add(vetor[0])
     
-    return a, b, c, alpha, beta, gamma,nat,tipos_atomicos,vetores
+    return a, b, c, alpha, beta, gamma,nat,tipos_atomicos,vetores, prefix 
 def calculo_parametros_lattice(a, b, c, alpha, beta, gamma):
-    # converter deg pra rad
+    """Cálcula parametros necessários para a criação do CELL_PARAMETERS.
+
+    Args:
+        a (float): Largura do paralelepípedo.
+        b (float): Comprimento do paralelepípedo.
+        c (float): Altura do paralelepípedo.
+        alpha (float): Ângulo entre as arestas b e c do paralelepípedo.
+        beta (float): Ângulo entre as arestas a e c do paralelepípedo.
+        gamma (float): Ângulo entre as arestas a e b do paralelepípedo.
+
+   
+    Returns:
+        tuple: Uma tupla contendo os seguintes valores calculados:
+            - ar (float): Largura do paralelepípedo no espaço reciproco.
+            - cgr (float): Cosseno do ângulo gamma no espaço reciproco.
+            - sgr (float): Seno do ângulo gamma no espaço reciproco.
+            - cb (float): Cosseno do ângulo beta.
+            - sa (float): Seno do ângulo alpha.
+            - ca (float): Cosseno do ângulo alpha.
+
+    Raises:
+        ValueError: Caso os valores dos ângulos fornecidos resultem em um valor de volume inválido (zero ou negativo).
+    """
     alpha = math.radians(alpha)
     beta = math.radians(beta)
     gamma = math.radians(gamma)
 
-    # c = cosseno, s = seno
     ca = math.cos(alpha)
     cb = math.cos(beta)
     cg = math.cos(gamma)
     sa = math.sin(alpha)
     sb = math.sin(beta)
-    sg = math.sin(gamma)
 
-    # Volume da célula real
     V = a * b * c * np.sqrt(1 - ca**2 - cb**2 - cg**2 + 2 * ca * cb * cg)
+    if V <= 0:
+        raise ValueError("Volume inválido.")
 
-    #valor de a no espaco reciproco
     ar = (b * c * sa) / V
 
-    #seno e cosseno de gamma reciproco
     cgr = (ca * cb - cg) / (sa * sb)
     sgr = math.sqrt(1 - cgr**2)
 
     return ar,cgr,sgr,cb,sa,ca
 def massa_atomica(elemento):
+    """Encontra a massa atômica de um Elemento.
+
+    Args:
+        elemento (str): Nome do elemento.
+
+    Returns:
+        float: Massa atomica do elemento.
+
+    Raises:
+        ValueError: Caso o elemento não esteja no dicionário.
+    """
     elemento_massa = {
         "H": 1.00784,
         "He": 4.002602,
@@ -178,23 +237,31 @@ def massa_atomica(elemento):
         "Pa": 231.03588,
         "U": 238.02891
     }
+    if elemento not in elemento_massa:
+        raise ValueError("Não há esse elemento no nosso banco de dados.")
     return elemento_massa[elemento]
 
-a, b, c, alpha, beta, gamma, nat, tipos_atomicos, vetores = extrair_parametros_rede('data_MP.txt')
+a, b, c, alpha, beta, gamma, nat, tipos_atomicos, vetores, prefix = extrair_parametros_rede('data_MP.txt')
 ar,cgr,sgr,cb,sa,ca = calculo_parametros_lattice(a, b, c, alpha, beta, gamma)
 diag = np.identity(3)
 stdbase = np.array([[1.0 / ar, -cgr / sgr / ar, cb * a], [0.0, b * sa, b * ca], [0.0, 0.0, c]],dtype=float,)
 base = np.dot(stdbase, diag)
 
-with open(f'{composto}.in', 'w') as file: #montar o .in do composto
-    file.write(f'&CONTROL\n/\n&SYSTEM\nibrav = 0\nnat = {nat}\nntyp ={len(tipos_atomicos)}\n/\n&ELECTRONS\n/\n')
+with open(f'{composto}.in', 'w') as file: 
+    file.write(f"&CONTROL\nprefix = '{prefix}'\n/\n&SYSTEM\nibrav = 0\nnat = {nat}\nntyp = {len(tipos_atomicos)}\n/\n&ELECTRONS\n/\n")
     file.write("CELL_PARAMETERS angstrom\n")
     for vetor in base:
-        file.write(f"   {vetor[0]:>10.8f}  {vetor[1]:>10.8f}  {vetor[2]:>10.8f}\n")
+        v_1 = Decimal(vetor[0]).quantize(Decimal('0.00000000'))
+        v_2 = Decimal(vetor[1]).quantize(Decimal('0.00000000'))
+        v_3 = Decimal(vetor[2]).quantize(Decimal('0.00000000'))
+        file.write(f"   {v_1:>10.8f}  {v_2:>10.8f}  {v_3:>10.8f}\n")
     file.write(f'ATOMIC_SPECIES\n')
     for elemento in tipos_atomicos:
         file.write(f'     {elemento} {massa_atomica(elemento)} {elemento}_pseudo\n')
     file.write('ATOMIC_POSITIONS {crystal}\n')
     for vetor in vetores:
-        file.write(f' {vetor[0]} {vetor[1]:>10.8f}  {vetor[2]:>10.8}  {vetor[3]:>10.8f}\n')
+        v_1 = Decimal(vetor[1]).quantize(Decimal('0.00000000'))
+        v_2 = Decimal(vetor[2]).quantize(Decimal('0.00000000'))
+        v_3 = Decimal(vetor[3]).quantize(Decimal('0.00000000'))
+        file.write(f' {vetor[0]} {v_1:>10.8f}  {v_2:>10.8f}  {v_3:>10.8f}\n')
     file.write('K_POINTS automatic')
