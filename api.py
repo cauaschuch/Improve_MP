e4010d6d04abfa0371880bdc56673daa0e41a7e6
Chavez-cu <phbbpedrobueno@usp.br>
2024-10-29 14:45:39 -0300

    buscar chemsys

--- api.py
+++ api.py
@@ -8,32 +8,36 @@ key = "H2RaVIDWeAR6N1y8E9lh9XYqB8mwVog7"
 
 composto = input('digite seu material ')
 s = input('Se quiser, digite a simetria ')
+chem = input('A-B-C ')
 
 with MPRester(key) as mpr:
     
     def busca_material(composto,simetria):
         sistemas = ['Triclinic', 'Monoclinic', 'Orthorhombic', 'Tetragonal', 'Trigonal', 'Hexagonal','Cubic']
-
-        if simetria in sistemas:
+        
+        if composto == '':
+            
+            docs = mpr.materials.summary.search(chemsys=chem)
+            mpids = [doc.material_id for doc in docs]            
+        
+        elif simetria in sistemas:
             docs = mpr.materials.summary.search(formula=composto,crystal_system=simetria)
             mpids = [doc.material_id for doc in docs]
-            mpids = mpids[0]
-            estrutura = mpr.get_structure_by_material_id(mpids)     
-            dados = mpr.materials.get_data_by_id(mpids)
-            internacional = dados.symmetry.number
-            
+            mpids = mpids[0] 
+
         else:
             docs = mpr.materials.summary.search(formula=composto)
             mpids = [doc.material_id for doc in docs]
             mpids = mpids[0]
             print ('\n !!!  simetria não especificada  !!!  \n')   
-            estrutura = mpr.get_structure_by_material_id(mpids)     
-            dados = mpr.materials.get_data_by_id(mpids)
-            internacional = dados.symmetry.number
-        
-        return internacional, estrutura
+        estrutura = mpr.get_structure_by_material_id(mpids)     
+        dados = mpr.materials.get_data_by_id(mpids)
+        estrut_nome = dados.symmetry.crystal_system      
+        internacional = dados.symmetry.number
 
-N_internacional,estrutura = busca_material(composto,s)
+        return internacional, estrutura, estrut_nome
+
+N_internacional,estrutura, estrut_nome = busca_material(composto,s)
 
 with open('data_MP.txt', 'w') as file:
     file.write(str(estrutura))
